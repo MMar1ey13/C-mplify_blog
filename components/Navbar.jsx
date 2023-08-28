17a34ff9ba530c7236cffd2bdf826d8c3bd50273
MMar1ey13 <mwendamarley12@gmail.com>
2023-08-28 23:57:31 +0300

    analytics

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,13 +1,14 @@
 // Navbar.js
 import React from 'react';
+import Link from 'next/link';
 
 const Navbar = () => {
   return (
     <nav className="bg-blue-500 p-4">
       <div className="container mx-auto">
-        <a href="/" className="text-white font-bold text-xl">
+        <Link href="/" className="text-white font-bold text-xl">
           My Website
-        </a>
+        </Link>
         <ul className="flex space-x-4">
           <li><a href="/" className="text-white hover:text-gray-200">Home</a></li>
           <li><a href="/about" className="text-white hover:text-gray-200">About</a></li>
--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,13 @@ import React, { useEffect, useState} from 'react';
 import { Layout, Navbar } from '@/components';
 import 'tailwindcss/tailwind.css'; 
 import '@/styles/globals.scss';
-import {Navbar } from '@/components';
 
 import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   return (
   <Layout>
+    <Navbar/>
   <Component {...pageProps} />
   <Analytics />
   </Layout>
